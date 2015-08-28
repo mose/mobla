500ebb21a58b3f9434a70b7730e16a4bd5615dfc
mose <mose@mose.com>
2015-08-28 17:29:38 +0800

    new post

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
+hugo
+
+git rm -r public/.gitmodules public/themes
+cp CNAME public
+
+git add .
+git commit -a -m'deploy'
+git push
+
 git subtree push --prefix=public git@github.com:mose/mobla.git gh-pages
